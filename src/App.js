194b5bb7c8812839518545a18af4265676bb5d1d
chalexan <cherednychenko.alexandr@gmail.com>
2021-05-10 13:20:45 +0300

    adding local express-server

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import BonusBar from "./components/BonusBar";
 
+const ClientID = "2c44d8c2-c89a-472e-aab3-9a8a29142315";
+const DeviceID = "7db72635-fd0a-46b9-813b-1627e3aa02ea";
+
 function App() {
   return (
     <>
       <div className="text-logo">
         <font size="4"> ЛОГОТИП </font>
       </div>
-      <BonusBar />
+      <BonusBar ClientID={ClientID} DeviceID={DeviceID} />
     </>
   );
 }
--- src/components/BonusBar.jsx
+++ src/components/BonusBar.jsx
@@ -1,11 +1,44 @@
-const BonusBar = () => {
+import { useEffect, useState } from "react";
+
+const BonusBar = (props) => {
+  useEffect(() => {
+    getDataFromAPI();
+  }, []);
+
+  const [currentQuantity, setcurrentQuantity] = useState(0);
+  const [forBurningQuantity, setforBurningQuantity] = useState(0);
+  const [dateBurning, setdateBurning] = useState(0);
+
+  const getDataFromAPI = async () => {
+    const urlToken = "http://localhost:8080/token";
+    const preResult = await fetch(urlToken, {
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        idClient: props.ClientID,
+        accessToken: "",
+        paramName: "device",
+        paramValue: props.DeviceID,
+        latitude: 0,
+        longitude: 0,
+        sourceQuery: 0,
+      }),
+    });
+    const result = await preResult.json();
+    setcurrentQuantity(result.fx.currentQuantity);
+    setforBurningQuantity(result.fx.forBurningQuantity);
+    setdateBurning(result.fx.dateBurning);
+  };
+
   return (
     <div className="bonus-block">
       <table>
         <tr>
           <td className="text-block">
             <font size="5">
-              <h4>300 бонусов</h4>
+              <h4>{currentQuantity} бонусов</h4>
+              {console.log(props)}
             </font>
           </td>
           <td className="arrow-block" rowspan="2">
@@ -15,7 +48,7 @@ const BonusBar = () => {
         <tr>
           <td className="text-block">
             <font className="text-description" color="grey">
-              29/03 сгорит 🔥 250 бонусов
+              {dateBurning} сгорит 🔥 {forBurningQuantity} бонусов
             </font>
           </td>
         </tr>
